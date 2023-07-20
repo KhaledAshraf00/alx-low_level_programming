1a190b47c45911201209712ff4c1bed1e1255ab7
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-07-20 04:47:33 -0700

    Functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - Draw straight line
+ * @n: number of times _ printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
