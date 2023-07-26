923523f731288618e3832b2d08551bc46ce27142
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-07-26 09:25:57 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,19 +6,20 @@
  */
 char *rot13(char *s)
 {
-int stringCount, rotation;
-char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'};
-char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
+int index, count;
+char r1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char r2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (index = 0; hi[index] != '\0'; ++index)
 {
-for (rotation = 0; rotation < 53; rotation++)
+for (count = 0; r1[count] != '\0' ; count++)
 {
-if (r1[rotation] == s[stringCount])
+if (hi[index] == r1[count])
 {
-s[stringCount] = r2[rotation];
+hi[index] = r2[count];
 break;
 }
 }
 }
-return (s);
+hi[index] = '\0';
+return (hi);
 }
