377e605c795427eb7627e5d4311af0d18f731edb
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-07-26 09:28:53 -0700

    Pointers and Arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,17 +9,17 @@ char *rot13(char *s)
 int index, count;
 char r1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char r2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-for (index = 0; hi[index] != '\0'; ++index)
+for (index = 0; s[index] != '\0'; ++index)
 {
 for (count = 0; r1[count] != '\0' ; count++)
 {
-if (hi[index] == r1[count])
+if (s[index] == r1[count])
 {
-hi[index] = r2[count];
+s[index] = r2[count];
 break;
 }
 }
 }
-hi[index] = '\0';
-return (hi);
+s[index] = '\0';
+return (s);
 }
