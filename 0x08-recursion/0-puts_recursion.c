6211a36c3fa7ddb985e51d993e55dae95371909d
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-08-02 12:48:37 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - Write a function that prints a string, followed by a new line
+ * _puts_recursion - Print a string followed by a new line
  * @s: This is the input string
  * Return: no return
  */
