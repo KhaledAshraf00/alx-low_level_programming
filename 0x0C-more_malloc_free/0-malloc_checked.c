9ab13312dd1bc477d1020c45db11cd8fedbe55e9
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-08-10 03:00:31 -0700

    Malloc and Free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * malloc_checked - Allocate memory using malloc
+ * @b: size of memory block
+ * Return: pointer to address
+ */
+void *malloc_checked(unsigned int b)
+{
+  int *block;
+  block = malloc(b);
+  if (block ==NULL)
+    exit(98);
+  return (block);
+}
