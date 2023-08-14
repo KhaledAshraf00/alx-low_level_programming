2ae9c60011a914d52fdf930c5b867fa2924b3c61
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-08-14 09:35:58 -0700

    Structs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**     
  * free_dog - free variable
  * @d: details
  */
-void free_dog(struct dog_t *d)
+void free_dog(dog_t *d)
 {
 if (d)
 {
