23513cebe663837bdede0f39ddfdbdf6791b5121
KhaledAshraf00 <khalidbedo2000@gmail.com>
2023-08-24 12:50:49 -0700

    Linked List

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - print a message before the main function
+ * Return: nothing
+ */
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
